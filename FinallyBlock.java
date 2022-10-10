d04cc4545e8f405d95d59d0c5ebb5bde5eec2ef9
VishnKore143 <vishnukore123@gmail.com>
2022-10-10 19:12:40 +0530

    Finally,Block Demo

---
+++ FinallyBlock.java
@@ -0,0 +1,12 @@
+package ExceptionHandling;
+
+public class FinallyBlock {
+    public static void main(String[] args) {
+        try {
+            System.out.println(10/0);
+        }
+        finally {
+            System.out.println("Finally message");
+        }
+    }
+}
